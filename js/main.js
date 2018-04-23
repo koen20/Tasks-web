7ed9c155725211f607a3860e12cf3ab74b64c462
Koen Habets <koenhabets1@gmail.com>
2018-04-24 00:17:17 +0200

    Tasks can now be completed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+var jsonArray = JSON.parse("[]");
+var userId;
+
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         console.log("signed in " + user.uid);
         handleSignedIn();
         databaseLoad(user.uid);
+        userId = user.uid;
     } else {
         console.log("signed out");
         $("#content").hide();
@@ -16,17 +20,43 @@ function databaseLoad(id) {
     ref.on('value', function (snapshot) {
         $("#taskItemsList").empty();
         snapshot.forEach(function (childSnapshot) {
+            var colorr;
+            if (childSnapshot.val().priority === 2) {
+                colorr = "style=\"color: #F44336;\"";
+            } else if (childSnapshot.val().priority === 1) {
+                colorr = "style=\"color: #FB8C00;\"";
+            }
             if (!childSnapshot.val().completed) {
-                var liContent = "<label class=\"mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect\" style=\'width: auto\'>\n" +
-                    "        <input type=\"checkbox\" class=\"mdl-checkbox__input\"/>\n" +
-                    "</label> " + childSnapshot.val().subject;
+                var liContent = "<span class=\"mdl-list__item-primary-content\">\n    <label class=\"mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect\" style=\'width: auto\'>\n       " +
+                    " <input type=\"checkbox\" class=\"mdl-checkbox__input\"/>\n    </label>\n" +
+                    "\n<span " + colorr + ">" + childSnapshot.val().subject + "</span>" +
+                    "<span class=\"mdl-list__item-sub-title\">" + getStringTs(childSnapshot.val().date) + "</span>\n</span>";
 
 
-                $("#taskItemsList").append("<li class=\"mdl-list__item\">" + liContent + "</li>");
+                $("#taskItemsList").append("<li class=\"mdl-list__item mdl-list__item--two-line\">" + liContent + "</li>");
                 componentHandler.upgradeDom();
+                var jsonObject = JSON.parse("{}");
+                jsonObject.subject = childSnapshot.val().subject;
+                jsonObject.date = childSnapshot.val().date;
+                jsonObject.id = childSnapshot.val().id;
+                jsonArray.push(jsonObject);
             }
         });
     });
+}
+
+function getStringTs(ts) {
+    var date = new Date(ts);
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var formattedTime = day + "-" + month + "-" + year + " " + hours + ':' + minutes.substr(-2);
+    if (ts === 0) {
+        formattedTime = "";
+    }
+    return formattedTime;
 }
 
 window.onload = function () {
@@ -34,3 +64,18 @@ window.onload = function () {
         firebase.auth().signOut();
     });
 };
+
+$("#taskItemsList").on("click", "input[type='checkbox']", function () {
+    if (this.checked) {
+        console.log("checked");
+        console.log($(this).parent().parent().children("span").text());
+        for(var i = 0; i < jsonArray.length; i++) {
+            var jsonObject = jsonArray[i];
+            if(jsonObject.subject === $(this).parent().parent().children("span").text()){
+                firebase.database().ref('users/' + userId + '/items/' + jsonObject.id).child("completed").set(true);
+            }
+        }
+    } else {
+        console.log("unchecked");
+    }
+});
